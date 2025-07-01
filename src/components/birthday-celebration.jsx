8df516c1452749880287906de57fe96960bbf681
stupidcupidy <hs3120749@gmail.com>
2025-07-01 23:54:26 +0530

    My qt

--- src/components/birthday-celebration.jsx
+++ src/components/birthday-celebration.jsx
@@ -26,7 +26,7 @@ export default function BirthdayCelebration() {
           <Sparkles className="w-8 h-8 text-yellow-500" />
           <Heart className="w-8 h-8 text-pink-500" />
         </div>
-        <h3 className="text-2xl sm:text-3xl font-bold text-center text-pink-600 mt-2">To khushi (moo-moo) qt </h3>
+        <h3 className="text-2xl sm:text-3xl font-bold text-center text-pink-600 mt-2">To khushi (moo-moo) my qt </h3>
       </motion.div>
 
       <motion.div
